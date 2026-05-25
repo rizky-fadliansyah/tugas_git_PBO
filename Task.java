ad4f1e2b668465537103a04ee59d8afc7441f7e5
Rizky <blogerrizky@gmail.com>
2026-05-25 20:27:07 +0700

    Add Task class dengan construktor dan getter

---
+++ Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private int id;
+    private String title;
+
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Task ID: " + id + " | Title: " + title;
+    }
+}
